c446d4c3c464987a2b2278c17480b4cf99e22a96
bujosa <davidbujosa@gmail.com>
2021-10-16 23:27:04 -0400

    Rename file

--- src/interfaces/invoices.interface.ts
+++ src/interfaces/invoices.interface.ts
@@ -1,9 +1,10 @@
 import { IDiscounts } from "./discounts.interface";
+import { IItem } from "./item.interface";
 import { IUser } from "./user.interface";
 
 export interface IInvoices {
   client: IUser;
   discount: IDiscounts;
-  items: IItems[];
+  items: IItem[];
   total: number;
 }
