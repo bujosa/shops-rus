ab8fbd65f974991f6bb53aed3b4bb5678fe861c2
bujosa <davidbujosa@gmail.com>
2021-10-18 00:57:52 -0400

    Fixed test

--- src/routes/discounts/__test__/discounts.test.ts
+++ src/routes/discounts/__test__/discounts.test.ts
@@ -1,39 +1,38 @@
 import request from "supertest";
 import * as faker from "faker";
 import { app } from "../../../app";
-import { Item } from "../../../models/item/item.entity";
-import { ItemType } from "../../../enums/item-type.enum";
+import { Discount } from "../../../models/discount/discount.entity";
 
 const buildEntity = async () => {
-  const entity = Item.build({
-    name: faker.name.findName(),
-    price: faker.datatype.number({ min: 100, max: 1000 }),
-    type: ItemType.FOOD,
+  const entity = Discount.build({
+    type: faker.lorem.word(),
+    discount: faker.datatype.number({ min: 0.01, max: 0.99 }),
+    percentage: true,
   });
 
   return await entity.save();
 };
 
-describe("GET /api/items", () => {
-  it("Returns all items", async () => {
+describe("GET /api/discounts", () => {
+  it("Returns all discounts", async () => {
     await buildEntity();
     await buildEntity();
     await buildEntity();
 
-    const { body: fetchedItems } = await request(app)
-      .get(`/api/items`)
+    const { body: fetchedDiscounts } = await request(app)
+      .get(`/api/discounts`)
       .send({})
       .expect(200);
 
-    expect(fetchedItems.length).toEqual(3);
+    expect(fetchedDiscounts.length).toEqual(3);
   });
 
   it("return empty array", async () => {
-    const { body: fetchedItems } = await request(app)
-      .get(`/api/items`)
+    const { body: fetchedDiscounts } = await request(app)
+      .get(`/api/discounts`)
       .send({})
       .expect(200);
 
-    expect(fetchedItems).toEqual([]);
+    expect(fetchedDiscounts).toEqual([]);
   });
 });
