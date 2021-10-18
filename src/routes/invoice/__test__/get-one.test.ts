d8fe8083b679cb63acf5891f9f0b7aa5d6ae942b
bujosa <davidbujosa@gmail.com>
2021-10-18 01:34:32 -0400

    Fixed get one test

--- src/routes/invoice/__test__/get-one.test.ts
+++ src/routes/invoice/__test__/get-one.test.ts
@@ -3,9 +3,22 @@ import * as faker from "faker";
 import { app } from "../../../app";
 import { Item } from "../../../models/item/item.entity";
 import { ItemType } from "../../../enums/item-type.enum";
+import { User } from "../../../models/user/user.entity";
+import { Invoice } from "../../../models/invoice/invoice.entity";
 
-const buildEntity = async () => {
-  const entity = Item.build({
+const buildUser = async () => {
+  const entity = await User.build({
+    fullName: faker.name.findName(),
+    affiliate: faker.datatype.boolean(),
+    employee: faker.datatype.boolean(),
+    createdAt: faker.date.past().toISOString(),
+  });
+
+  return await entity.save();
+};
+
+const buildItem = async () => {
+  const entity = await Item.build({
     name: faker.name.findName(),
     price: faker.datatype.number({ min: 100, max: 1000 }),
     type: ItemType.FOOD,
@@ -14,28 +27,41 @@ const buildEntity = async () => {
   return await entity.save();
 };
 
-describe("GET /api/item/:id", () => {
-  it("fetches the item", async () => {
-    const item = await buildEntity();
+const buildEntity = async () => {
+  const user = await buildUser();
+  const item = await buildItem();
 
-    const { body: fetchedItem } = await request(app)
-      .get(`/api/items/${item.id}`)
+  const entity = await Invoice.create({
+    client: user.id,
+    items: [item.id],
+    total: faker.datatype.number({ min: 100, max: 1000 }),
+  });
+
+  return await entity.save();
+};
+
+describe("GET /api/invoices/:id", () => {
+  it("fetches the invoice", async () => {
+    const invoice = await buildEntity();
+
+    const { body: fetchedInvoice } = await request(app)
+      .get(`/api/invoices/${invoice.id}`)
       .send({})
       .expect(200);
 
-    expect(fetchedItem.name).toEqual(item.name);
+    expect(fetchedInvoice.id).toEqual(invoice.id);
   });
 
   it("return status code 404", async () => {
     await request(app)
-      .get(`/api/items/${faker.lorem.word()}`)
+      .get(`/api/invoices/${faker.lorem.word()}`)
       .send({})
       .expect(404);
   });
 
   it("throw message Not Found Error", async () => {
     await request(app)
-      .get(`/api/items/${faker.lorem.word()}`)
+      .get(`/api/invoices/${faker.lorem.word()}`)
       .send({})
       .expect({ errors: [{ message: "Not Found" }] });
   });
