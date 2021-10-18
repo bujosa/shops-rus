081871109010540595c91c5098055dccf7338c3d
bujosa <davidbujosa@gmail.com>
2021-10-17 20:46:54 -0400

    Add get one item

---
+++ src/routes/item/get-one.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import { NotFoundError } from "../../errors/not-found-error";
+import { Item } from "../../models/item/item.entity";
+
+const getOneItemRouterById = express.Router();
+
+getOneItemRouterById.get(
+  "/api/items/:id",
+  async (req: Request, res: Response) => {
+    const item = await Item.findById(req.params.id);
+
+    if (!item) {
+      throw new NotFoundError();
+    }
+
+    return res.send(item);
+  }
+);
+
+export { getOneItemRouterById };
