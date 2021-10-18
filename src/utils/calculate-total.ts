ac948f4a6100981ee7fc5c19990dd314a9a6b483
bujosa <davidbujosa@gmail.com>
2021-10-17 21:49:22 -0400

    Fixed first param type

--- src/utils/calculate-total.ts
+++ src/utils/calculate-total.ts
@@ -1,7 +1,7 @@
 import { IItem } from "../interfaces/item.interface";
 import { IUser } from "../interfaces/user.interface";
 
-export const calculateTotal = (products: IItem, client: IUser): number => {
+export const calculateTotal = (products: IItem[], client: IUser): number => {
   let total = 0;
 
   return total;
