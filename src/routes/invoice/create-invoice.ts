18a792cbdc216cce234126030e378450b566fe20
bujosa <davidbujosa@gmail.com>
2021-10-17 21:49:13 -0400

    Modified function

--- src/routes/invoice/create-invoice.ts
+++ src/routes/invoice/create-invoice.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import { Schema } from "mongoose";
+import { NotFoundError } from "../../errors/not-found-error";
+import { IItem } from "../../interfaces/item.interface";
 import { validateRequest } from "../../middlewares/validate-request";
 import { Invoice } from "../../models/invoice/invoice.entity";
 import { Item } from "../../models/item/item.entity";
 import { User } from "../../models/user/user.entity";
+import { calculateTotal } from "../../utils/calculate-total";
 
 const router = express.Router();
 
@@ -17,32 +21,38 @@ router.post(
       .isArray()
       .isUUID()
       .withMessage("items is required"),
-    body("discount")
-      .not()
-      .isEmpty()
-      .isArray()
-      .isUUID()
-      .withMessage("discount is required"),
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    const { client: clientId, items, discount } = req.body;
+    const { client: clientId, items } = req.body;
 
-    const products = [];
+    const products: IItem[] = [];
 
     const client = await User.findById(clientId);
 
-    items.forEach(async (item: string) => {
+    if (!client) {
+      throw new NotFoundError();
+    }
+
+    items.forEach(async (item: Schema.Types.ObjectId) => {
       const product = await Item.findById(item);
-      products.push(product);
+
+      if (product) {
+        products.push({
+          name: product.name,
+          price: product.price,
+          type: product.type,
+        });
+      } else {
+        throw new NotFoundError();
+      }
     });
 
-    let total = 0;
+    const total = calculateTotal(products, client);
 
     const invoice = Invoice.build({
       client: clientId,
       items,
-      discount,
       total,
     });
 
