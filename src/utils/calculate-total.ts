b250b15ce74200b0553c4a339c620739162b03ab
bujosa <davidbujosa@gmail.com>
2021-10-17 22:20:57 -0400

    Finished calculate total

--- src/utils/calculate-total.ts
+++ src/utils/calculate-total.ts
@@ -4,5 +4,29 @@ import { IUser } from "../interfaces/user.interface";
 export const calculateTotal = (products: IItem[], client: IUser): number => {
   let total = 0;
 
-  return total;
+  products.forEach((product) => {
+    if (productType(product)) {
+      return;
+    }
+
+    if (client.affiliate && client.employee) {
+      product.price = product.price * 0.7;
+    } else if (client.affiliate) {
+      product.price = product.price * 0.9;
+    } else if (client.employee) {
+      product.price = product.price * 0.7;
+    }
+
+    total += product.price;
+  });
+
+  return total - Math.floor(total / 100) * 5;
+};
+
+const productType = (product: IItem): boolean => {
+  if (product.type === "food") {
+    return true;
+  } else {
+    return false;
+  }
 };
