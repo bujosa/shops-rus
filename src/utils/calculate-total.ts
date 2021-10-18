0128ba8601ad9e12687f4ec8b13a43ae23c39b9a
bujosa <davidbujosa@gmail.com>
2021-10-17 21:42:26 -0400

    Add calculate total

---
+++ src/utils/calculate-total.ts
@@ -0,0 +1,8 @@
+import { IItem } from "../interfaces/item.interface";
+import { IUser } from "../interfaces/user.interface";
+
+export const calculateTotal = (products: IItem, client: IUser): number => {
+  let total = 0;
+
+  return total;
+};
