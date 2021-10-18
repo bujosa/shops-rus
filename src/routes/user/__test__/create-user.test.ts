11163b43c50fc3298853a355503db5eddf02162f
bujosa <davidbujosa@gmail.com>
2021-10-18 00:04:36 -0400

    Add create user test

--- src/routes/user/__test__/create-user.test.ts
+++ src/routes/user/__test__/create-user.test.ts
@@ -1,63 +1,32 @@
 import request from "supertest";
+import * as faker from "faker";
 import { app } from "../../../app";
 
-it("Has a route handler listening to /api/tickets fro post request", async () => {
-  const response = await request(app).post("/api/tickets").send({});
+describe("POST /api/users", () => {
+  it("create user", async () => {
+    const user = {
+      fullName: faker.name.findName(),
+      affiliate: faker.datatype.boolean(),
+      employee: faker.datatype.boolean(),
+      createdAt: faker.date.past().toISOString(),
+    };
 
-  expect(response.status).not.toEqual(404);
-});
+    const { body: fetchedUser } = await request(app)
+      .post(`/api/users`)
+      .send(user)
+      .expect(201);
 
-it("return an error if an invalid title is provided", async () => {
-  await request(app)
-    .post("/api/tickets")
-    .send({
-      title: "",
-      price: 10,
-    })
-    .expect(400);
+    expect(fetchedUser.fullName).toEqual(user.fullName);
+  });
 
-  await request(app)
-    .post("/api/tickets")
-    .send({
-      price: 10,
-    })
-    .expect(400);
-});
+  it(" returns a 400 an invalid affiliate", async () => {
+    const user = {
+      fullName: faker.name.findName(),
+      affiliate: faker.datatype.boolean(),
+      employee: faker.lorem.word(),
+      createdAt: faker.date.past().toISOString(),
+    };
 
-it("return an error if an invalid price is provided", async () => {
-  await request(app)
-    .post("/api/tickets")
-    .send({
-      title: "title",
-      price: -10,
-    })
-    .expect(400);
-
-  await request(app)
-    .post("/api/tickets")
-    .send({
-      title: "test",
-    })
-    .expect(400);
-});
-
-it("creates a user with valid inputs", async () => {
-  let tickets = await Ticket.find({});
-  expect(tickets.length).toEqual(0);
-
-  const title = "asldkfj";
-
-  await request(app)
-    .post("/api/tickets")
-    .set("Cookie", global.signin())
-    .send({
-      title,
-      price: 20,
-    })
-    .expect(201);
-
-  tickets = await Ticket.find({});
-  expect(tickets.length).toEqual(1);
-  expect(tickets[0].price).toEqual(20);
-  expect(tickets[0].title).toEqual(title);
+    request(app).post("/api/users").send(user).expect(400);
+  });
 });
