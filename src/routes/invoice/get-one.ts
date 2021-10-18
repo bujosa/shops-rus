ffb1097e2c016b050a0460fa4e1d6679c6b85084
bujosa <davidbujosa@gmail.com>
2021-10-18 01:08:11 -0400

    Replace find by id for find one

--- src/routes/invoice/get-one.ts
+++ src/routes/invoice/get-one.ts
@@ -1,14 +1,13 @@
 import express, { Request, Response } from "express";
 import { NotFoundError } from "../../errors/not-found-error";
 import { Invoice } from "../../models/invoice/invoice.entity";
-import { Item } from "../../models/item/item.entity";
 
 const getOneInvoiceRouterById = express.Router();
 
 getOneInvoiceRouterById.get(
   "/api/invoices/:id",
   async (req: Request, res: Response) => {
-    const invoice = await Invoice.findById(req.params.id);
+    const invoice = await Invoice.findOne({ id: req.params.id });
 
     if (!invoice) {
       throw new NotFoundError();
