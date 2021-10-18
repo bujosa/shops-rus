953adf37169fd02d99f2e4ee02a7341c31de50d1
bujosa <davidbujosa@gmail.com>
2021-10-18 00:57:57 -0400

    Fixed test

--- src/routes/discounts/__test__/create-discount.test.ts
+++ src/routes/discounts/__test__/create-discount.test.ts
@@ -1,32 +1,30 @@
 import request from "supertest";
 import * as faker from "faker";
 import { app } from "../../../app";
-import { IItem } from "../../../interfaces/item.interface";
-import { ItemType } from "../../../enums/item-type.enum";
+import { IDiscount } from "../../../interfaces/discount.interface";
 
-describe("POST /api/items", () => {
-  it("create item", async () => {
-    const item: IItem = {
-      name: faker.name.findName(),
-      price: faker.datatype.number({ min: 100, max: 1000 }),
-      type: ItemType.FOOD,
+describe("POST /api/discounts", () => {
+  it("create discount", async () => {
+    const discount: IDiscount = {
+      type: faker.lorem.word(),
+      discount: faker.datatype.number({ min: 0.01, max: 0.99 }),
+      percentage: true,
     };
 
-    const { body: fetchedItem } = await request(app)
-      .post(`/api/items`)
-      .send(item)
+    const { body: fetchedDiscount } = await request(app)
+      .post(`/api/discounts`)
+      .send(discount)
       .expect(201);
 
-    expect(fetchedItem.name).toEqual(item.name);
+    expect(fetchedDiscount.type).toEqual(discount.type);
   });
 
   it(" returns a 400 an invalid type", async () => {
-    const item: IItem = {
-      name: faker.name.findName(),
-      price: faker.datatype.number({ min: 100, max: 1000 }),
+    const discount = {
       type: faker.lorem.word(),
+      discount: faker.datatype.number({ min: 0.01, max: 0.99 }),
+      percentage: faker.lorem.word(),
     };
-
-    await request(app).post("/api/items").send(item).expect(400);
+    await request(app).post("/api/discounts").send(discount).expect(400);
   });
 });
