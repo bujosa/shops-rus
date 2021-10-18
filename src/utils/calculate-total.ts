274c8c66a2c0b03b15ded036486ca7285a743d72
bujosa <davidbujosa@gmail.com>
2021-10-18 03:33:25 -0400

    Add comment on calculate total

--- src/utils/calculate-total.ts
+++ src/utils/calculate-total.ts
@@ -4,6 +4,9 @@ import { IUser } from "../interfaces/user.interface";
 export const calculateTotal = (products: IItem[], client: IUser): number => {
   let total = 0;
 
+  const date = new Date(client.createdAt);
+  const year = date.getFullYear();
+
   products.forEach((product) => {
     if (productType(product)) {
       total += product.price;
@@ -11,13 +14,18 @@ export const calculateTotal = (products: IItem[], client: IUser): number => {
     }
 
     if (client.affiliate && client.employee) {
+      // if the client is an affiliate and an employee of the company, the biggest discount is taken
       product.price = product.price * 0.7;
     } else if (client.affiliate) {
+      // if the client is an affiliate, the price is multiplied by 0.9
       product.price = product.price * 0.9;
     } else if (client.employee) {
+      // if the client is an employee, the price is multiplied by 0.7
       product.price = product.price * 0.7;
+    } else if (year + 2 <= new Date().getFullYear()) {
+      // If the client is older than 2 years
+      product.price = product.price * 0.95;
     }
-
     total += product.price;
   });
 
