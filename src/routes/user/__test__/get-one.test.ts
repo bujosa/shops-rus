23f9f5380e5fa37f9d425082ee5b705d8765b316
bujosa <davidbujosa@gmail.com>
2021-10-17 23:00:20 -0400

    Add new test

---
+++ src/routes/user/__test__/get-one.test.ts
@@ -0,0 +1,30 @@
+import request from "supertest";
+import * as faker from "faker";
+import { User } from "../../../models/user/user.entity";
+import { app } from "../../../app";
+
+const buildEntity = async () => {
+  const entity = User.build({
+    fullName: faker.name.findName(),
+    affiliate: faker.datatype.boolean(),
+    employee: faker.datatype.boolean(),
+    createdAt: faker.date.past().toISOString(),
+  });
+
+  return await entity.save();
+};
+
+describe("GET /user/:id", () => {
+  it("fetches the user", async () => {
+    const user = await buildEntity();
+
+    console.log(user);
+
+    const { body: fetchedUser } = await request(app)
+      .get(`/api/users/:${user.id}`)
+      .send()
+      .expect(200);
+
+    expect(fetchedUser.id).toEqual(user.id);
+  });
+});
